3130da781062406cad826c28f1bc36b6465b2ca3
Jacob Bruun <jbruun@huronconsultinggroup.com>
2016-09-19 16:47:06 -0700

    Removes psst extension from angular feature templates.

--- src/NewAngularFeature.ps1
+++ src/NewAngularFeature.ps1
@@ -36,11 +36,11 @@ function New-AngularFeature
     $Model.Module.Metadata.Imports += "CommonModule"
 
     # Add the module to the bundle export list
-    $Model.Bundle.Exports += Expand-Template -InputFile "$TemplateDir\index.export.psst" -Path "./$($Model.Name.KebabCase).module"
+    $Model.Bundle.Exports += Expand-Template -InputFile "$TemplateDir\index.export.ts" -Path "./$($Model.Name.KebabCase).module"
 
     if ($Routing) {
         # Add the routing module to the bundle export list
-        $Model.Bundle.Exports = Expand-Template -InputFile "$TemplateDir\index.export.psst" -Path "./$($Model.Name.KebabCase).routing" 
+        $Model.Bundle.Exports = Expand-Template -InputFile "$TemplateDir\index.export.ts" -Path "./$($Model.Name.KebabCase).routing" 
 
         # Import the routing module and providers
         $Model.Module.Imports += New-AngularImport -Imports @("$($Model.Name)Routing","$($Model.Name)RoutingProviders") -Path "./$($Model.Name.KebabCase).routing"
@@ -50,19 +50,19 @@ function New-AngularFeature
 
     # Create the module file
     Expand-Template `
-        -InputFile "$TemplateDir\module.ts.psst" `
+        -InputFile "$TemplateDir\module.ts" `
         -OutputFile "$($Model.Name.KebabCase)\$($Model.Name.KebabCase).module.ts" `
         -Model $Model.Module
     
     # Create a new bundle file to export the feature contents
-    Expand-Template -InputFile "$TemplateDir\index.ts.psst" `
+    Expand-Template -InputFile "$TemplateDir\index.ts" `
         -OutputFile "$($Model.Name.KebabCase)\index.ts" `
         -Model $Model.Bundle
 
     # Create the routing file 
     if ($Routing) {
         Expand-Template `
-            -InputFile "$TemplateDir\routing.ts.psst" `
+            -InputFile "$TemplateDir\routing.ts" `
             -OutputFile "$($Model.Name.KebabCase)\$($Model.Name.KebabCase).routing.ts" `
             -Model $Model
     }
