84a8c42cc1b8862903edf7b974c0e6b6305045e6
Jake Bruun <jake.bruun@gmail.com>
2016-10-31 22:39:04 -0700

    Include all psake task files by convention.
    Adds command to find all the files suffixed with 'task.ps1' within the psake/ directory structure and load them into the script context.

--- src/PsstGeneratorModule/Psst.$($Model.Name)/psake/api/tasks.ps1
+++
@@ -1,4 +0,0 @@
-. `$PSScriptRoot\remove-api-key.task.ps1
-. `$PSScriptRoot\store-api-key.task.ps1
-. `$PSScriptRoot\show-api-key.task.ps1
-. `$PSScriptRoot\show-full-api-key.task.ps1
--- src/PsstGeneratorModule/Psst.$($Model.Name)/psake/tasks.ps1
+++ src/PsstGeneratorModule/Psst.$($Model.Name)/psake/tasks.ps1
@@ -1,17 +1,4 @@
 . `$PSScriptRoot\conditions.ps1
 . `$PSScriptRoot\settings.ps1
 
-. `$PSScriptRoot\api\tasks.ps1
-
-. `$PSScriptRoot\default.task.ps1
-. `$PSScriptRoot\build.task.ps1
-. `$PSScriptRoot\init.task.ps1
-. `$PSScriptRoot\clean.task.ps1
-. `$PSScriptRoot\update-module-manifest.task.ps1
-. `$PSScriptRoot\build-impl.task.ps1
-. `$PSScriptRoot\test.task.ps1
-. `$PSScriptRoot\publish.task.ps1
-. `$PSScriptRoot\compress-templates.task.ps1
-. `$PSScriptRoot\zip-artifacts.task.ps1
-. `$PSScriptRoot\install.task.ps1
-. `$PSScriptRoot\uninstall.task.ps1
+Get-ChildItem `$PSScriptRoot -Filter *.task.ps1 -Recurse | ForEach-Object { . `$_.FullName }
