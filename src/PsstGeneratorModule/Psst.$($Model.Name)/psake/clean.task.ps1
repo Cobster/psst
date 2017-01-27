60772944a6e371238beafb385b90150b11a29882
Jake Bruun <jake.bruun@gmail.com>
2017-01-27 08:06:00 -0800

    Enhance the templated clean task.

--- src/PsstGeneratorModule/Psst.$($Model.Name)/psake/clean.task.ps1
+++ src/PsstGeneratorModule/Psst.$($Model.Name)/psake/clean.task.ps1
@@ -3,14 +3,22 @@
 #
 Task 'clean' ``
     -description "Deletes the contents of the release directory." ``
-    -requiredVariables ReleaseDir, TemplateCache ``
+    -requiredVariables ModuleName, ReleaseDir, TemplateCache ``
 {
     if (Test-Path `$ReleaseDir) {
-        Get-ChildItem `$ReleaseDir | Remove-Item -Recurse -Force -Verbose:`$VerbosePreference
+        Get-ChildItem `$ReleaseDir | ForEach-Object {
+            Write-Host "Removing `$(`$_.FullName)"
+            Remove-Item -Path `$_.FullName -Recurse -Force -Verbose:`$VerbosePreference
+        }
+    }
+
+    if (Get-Module -Name `$ModuleName) {
+        Write-Host "Removing module: `$ModuleName"
+        Remove-Module `$ModuleName
     }
 
     if ((Test-Path `$TemplateCache)) {
         Write-Host "Deleting template cache at `$TemplateCache"
-        Remove-Item `$TemplateCache -Force
+        Remove-Item `$TemplateCache -Force -Recurse
     }
 }
