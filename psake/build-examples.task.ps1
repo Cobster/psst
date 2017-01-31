cb26aa5f21550513da7ad7446b816610da41df05
Jake Bruun <jake.bruun@gmail.com>
2017-01-30 19:35:41 -0800

    Updates build example task to use invoke verb.

--- psake/build-examples.task.ps1
+++ psake/build-examples.task.ps1
@@ -1,7 +1,7 @@
 Task 'build-examples' `
-    -requiredVariables ExamplesDir, ModuleName, ReleaseDir `
+    -requiredVariables ExamplesDir, ModuleName, ReleaseDir, Version `
     -precondition { BuildOutputExists -and ModuleIsImported } `
 {
     Write-Host "Creating new Psst.Azure module"
-    New-PsstGeneratorModule -Name "Example" -OutputPath $ExamplesDir
+    Invoke-PsstGeneratorModule -Name "Example" -OutputPath $ExamplesDir -Version $Version
 }
